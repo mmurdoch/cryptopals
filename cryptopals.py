a7e664acabddcb36666bafd93ebefe057bdc1eb4
Matthew Murdoch <matthew.murdoch.0@gmail.com>
2017-05-24 07:23:24 +0100

    Solve challenge 2

--- cryptopals.py
+++ cryptopals.py
@@ -1,6 +1,6 @@
 import array
 import base64
-
+import binascii
 
 def byte_array():
     return array.array('B')
@@ -9,7 +9,7 @@ def hex_to_bytes(hex_string):
     return array.array('B', hex_string.decode('hex'))
 
 def bytes_to_hex(bytes):
-    return bytes.encode('hex')
+    return binascii.hexlify(bytes)
 
 def bytes_to_base64(bytes):
     return base64.b64encode(bytes)
@@ -36,4 +36,4 @@ def assert_equal(expected, actual):
 assert_equal('SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t', hex_to_base64('49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d'))
 
 # Set 1, Challenge 2
-#assert_equal('746865206b696420646f6e277420706c6179', xor('1c0111001f010100061a024b53535009181c', '686974207468652062756c6c277320657965'))
+assert_equal('746865206b696420646f6e277420706c6179', xor('1c0111001f010100061a024b53535009181c', '686974207468652062756c6c277320657965'))
